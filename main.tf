2b1b8681b27dfcfd7b5568f487e5bff32f005cbd
dimpz1804 <dimpy.chugh@hashicorp.com>
2026-07-22 18:55:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -65,6 +65,8 @@ resource "aws_db_parameter_group" "education" {
   }
 }
 
+
+
 resource "aws_db_instance" "education" {
   identifier             = "${var.db_name}-${random_pet.random.id}"
   instance_class         = "db.t3.micro"
